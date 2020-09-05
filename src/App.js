821186cb5c8baa2b77f4297d425fdc8d57a2ebda
Alex Hand <alex.j.hand@gmail.com>
2020-09-04 20:16:17 -0500

    Imported HomePage

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import './App.css'
 
 import Footer from './components/Footer'
 import Header from './components/Header'
+import HomePage from './components/HomePage'
 
 const Main = () => (
 	<main>
 		<Switch>
-
+			<Route exact path='/' component={HomePage} />
 		</Switch>
 	</main>
 )
