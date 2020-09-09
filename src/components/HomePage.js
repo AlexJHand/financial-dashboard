874f3a51d067e546322ef742a24bcf2d76e06181
Alex Hand <alex.j.hand@gmail.com>
2020-09-08 21:41:36 -0500

    Added in basic axios call

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,5 +2,36 @@ import React from 'react'
 import axios from 'axios'
 
 export default class HomePage extends React.Component {
-    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            dia: ""
+        }
+
+        this.getDia = this.getDia.bind(this)
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount")
+        this.getDia()
+    }
+
+    getDia() {
+        const options = {
+            method: 'get',
+            url: 'https://api.tiingo.com/tiingo/daily/dia/prices',
+            authorization: `Token ${process.env.TIINGO_TOKEN}`
+        }
+        axios(options)
+            .then(results => console.log("results -> ", results))
+    }
+
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
 }
