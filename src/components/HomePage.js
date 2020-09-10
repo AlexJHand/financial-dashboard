0dacfe33e0c0b763eb0853dee3db8d7133c87212
Alex Hand <alex.j.hand@gmail.com>
2020-09-09 19:56:00 -0500

    Removed dotenv

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import axios from 'axios'
-require('dotenv').config()
 
 export default class HomePage extends React.Component {
     constructor(props) {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-require('dotenv').config()
 
 ReactDOM.render(
 	<BrowserRouter >
