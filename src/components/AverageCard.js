db59bb86e492adcd2e31f68e6810b289cc350b2a
Alex Hand <alex.j.hand@gmail.com>
2020-09-28 22:11:47 -0500

    Basic boilerplate for AverageCard

---
+++ src/components/AverageCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default class AverageCard extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: "",
+            close: "",
+            difference: ""
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
